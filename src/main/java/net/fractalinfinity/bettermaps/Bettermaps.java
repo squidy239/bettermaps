16bb7e0f9d94cb3ad80cee47f89cd858e2d7d3e8
sacha <sachabarsayuracko@gmail.com>
2024-04-25 21:05:23 -0500

    Reapply "changed basicly evrything, massive performence improvements."
    This reverts commit 6fa1d09e988724e130716a5c362da46a72c181ee.

--- src/main/java/net/fractalinfinity/bettermaps/Bettermaps.java
+++ src/main/java/net/fractalinfinity/bettermaps/Bettermaps.java
@@ -18,10 +18,16 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public final class Bettermaps extends JavaPlugin implements Listener {
     JSONObject bufferdict = new JSONObject();
+
     JSONObject framedict = new JSONObject();
+    ScheduledExecutorService scheduler
+            = Executors.newScheduledThreadPool(8);
 
     @Override
     public void onEnable() {
@@ -37,27 +43,6 @@ public final class Bettermaps extends JavaPlugin implements Listener {
     }
 
 
-    private void setiimgbufferfromvid(int id, String path) {
-        if (Files.exists(Paths.get(path + "/vids/" + id))) {
-            try {
-                int f;
-                if (framedict.containsKey(id)) {
-                    f = (int) framedict.get(id);
-                } else {
-                    f = 0;
-                }
-                bufferdict.put(id, ImageIO.read(new File(path + "/vids/" + id + "/" + f + ".png")));
-                int e = new File(path + "/vids/" + id).list().length;
-                if (f >= e - 1) {
-                    f = 0;
-                }
-                framedict.put(id, f + 1);
-            } catch (IOException ignored) {
-            }
-        }
-
-    }
-
     private void setiimgbufferfromfile(int id, String path) {
         if (Files.exists(Paths.get(path + "/images/" + id + ".png")) && !Files.exists(Paths.get(path + "/vids/" + id))) {
             try {
@@ -71,31 +56,54 @@ public final class Bettermaps extends JavaPlugin implements Listener {
     @EventHandler
     public void mapevent(MapInitializeEvent mapp) {
         int id = mapp.getMap().getId();
-        System.out.println("map id " + id + " initalized");
+        getLogger().info("map id " + id + " initalized");
         Player[] playr = {null};
+        MapCanvas[] mc = {null};
         MapView[] mapview = {null};
+        boolean[] onn = {true};
         String imgpath = "./mapimg";
-        Bukkit.getScheduler().runTaskTimer(this,()->{if(playr[0] == null){System.out.print("null");}else{playr[0].sendMap(mapview[0]);}},1,1);
-        Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> setiimgbufferfromvid(id, imgpath), 1, 1);
-        Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> setiimgbufferfromfile(id, imgpath), 1, 20);
+        int[] m = {1};
+        if (new File(imgpath + "/vids/" + id).exists()){m[0] = new File(imgpath + "/vids/" + id).listFiles().length;}
+        Bukkit.getScheduler().runTaskTimer(this, () -> playr[0].sendMap(mapview[0]), 1, 1);
+        scheduler.scheduleWithFixedDelay(()->{if (new File(imgpath + "/vids/" + id).exists()){onn[0] = true;m[0] = new File(imgpath + "/vids/" + id).listFiles().length;}},5000,5000,TimeUnit.SECONDS);
+        scheduler.scheduleWithFixedDelay(() -> setiimgbufferfromfile(id, imgpath), 500, 500, TimeUnit.MILLISECONDS);
         mapp.getMap().getRenderers().clear();
         mapp.getMap().addRenderer(new MapRenderer() {
             @Override
             public void render(@NotNull MapView mapView, @NotNull MapCanvas mapCanvas, @NotNull Player player) {
-                if (bufferdict.containsKey(id) && framedict.containsKey(id)) {
-                    try {
-                        mapCanvas.drawImage(0, 0, (BufferedImage) bufferdict.get(id));
-                    } catch (Exception e){
-                        throw new RuntimeException(e);
-                    }
+                if (onn[0] == true) {
                     playr[0] = player;
                     mapview[0] = mapView;
-                } else if (bufferdict.containsKey(id)) {
-                    mapCanvas.drawImage(0, 0, (BufferedImage) bufferdict.get(id));
+                    onn[0] = false;
+                    mc[0] = mapCanvas;
                 }
             }
         });
-
+        scheduler.scheduleAtFixedRate(() -> {
+            if (Files.exists(Paths.get(imgpath + "/vids/" + id))) {
+                try {
+                    int f;
+                    boolean t = true;
+                    BufferedImage bb;
+                    if (framedict.containsKey(id)) {
+                        f = (int) framedict.get(id);
+                    } else {
+                        f = 0;
+                    }
+                    try{
+                        bb = ImageIO.read(new File(imgpath + "/vids/" + id + "/" + f + ".png")).getSubimage(0, 0, 128, 128); }
+                    catch (Exception e){t = false;getLogger().warning("error reading image, last frame: "+f+"max frame:"+m[0]);throw new RuntimeException(e);}
+                    if(t){mc[0].drawImage(0, 0, bb);}
+                    if (f >= m[0] - 1) {
+                        //System.out.println("m[0]: "+m[0]);
+                        f = 0;
+                    }
+                    framedict.put(id, f + 1);
+                } catch (Exception e) {throw new RuntimeException(e);}
+            } else if (bufferdict.containsKey(id)) {
+                mc[0].drawImage(0, 0, (BufferedImage) bufferdict.get(id));
+            }
+        }, 1000, 50, TimeUnit.MILLISECONDS);
 
     }
 
