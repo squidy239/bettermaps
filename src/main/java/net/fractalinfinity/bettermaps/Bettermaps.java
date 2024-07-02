564de1910df8a8d0d92b359d1b3f18cd7ec7949c
sacha <sachabarsayuracko@gmail.com>
2024-07-01 22:02:09 -0500

    fixed bugs and improvements

--- src/main/java/net/fractalinfinity/bettermaps/Bettermaps.java
+++ src/main/java/net/fractalinfinity/bettermaps/Bettermaps.java
@@ -1,7 +1,7 @@
 package net.fractalinfinity.bettermaps;
 
 
-import net.coobird.thumbnailator.Thumbnailator;
+import net.coobird.thumbnailator.Thumbnails;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -83,17 +83,23 @@ public final class Bettermaps extends JavaPlugin implements Listener {
         String imgpath = "./mapimg";
         int[] m = {1};
         setiimgbufferfromfile(id, imgpath);
-        if (new File(imgpath + "/vids/" + id).exists()){m[0] = new File(imgpath + "/vids/" + id).listFiles().length;}
+        if (new File(imgpath + "/vids/" + id).exists()) {
+            m[0] = new File(imgpath + "/vids/" + id).listFiles().length;
+        }
         //Bukkit.getScheduler().runTaskTimer(this, () -> playr[0].sendMap(mapview[0]), 1, 1);
-        scheduler.scheduleWithFixedDelay(()->{if (new File(imgpath + "/vids/" + id).exists()){m[0] = new File(imgpath + "/vids/" + id).listFiles().length;}},20,20,TimeUnit.MILLISECONDS);
+        scheduler.scheduleWithFixedDelay(() -> {
+            if (new File(imgpath + "/vids/" + id).exists()) {
+                m[0] = new File(imgpath + "/vids/" + id).listFiles().length;
+            }
+        }, 20, 20, TimeUnit.MILLISECONDS);
         scheduler.scheduleWithFixedDelay(() -> setiimgbufferfromfile(id, imgpath), 20, 20, TimeUnit.SECONDS);
         mapp.getMap().getRenderers().clear();
         mapp.getMap().addRenderer(new MapRenderer() {
             @Override
             public void render(@NotNull MapView mapView, @NotNull MapCanvas mapCanvas, @NotNull Player player) {
-                    //playr[0] = player;
-                    //mapview[0] = mapView;
-                    mc[0] = mapCanvas;
+                //playr[0] = player;
+                //mapview[0] = mapView;
+                mc[0] = mapCanvas;
             }
         });
         scheduler.scheduleAtFixedRate(() -> {
@@ -106,17 +112,21 @@ public final class Bettermaps extends JavaPlugin implements Listener {
                     } else {
                         f = 0;
                     }
-                    try{
-                        bb = ImageIO.read(new File(imgpath + "/vids/" + id + "/" + f + ".png")).getSubimage(0, 0, 128, 128); }
-                    catch (Exception e){
-                        getLogger().warning("error reading image, last frame: "+f+"max frame:"+m[0]);}
-                    mc[0].drawImage(0, 0, bb);
+                    try {
+                        bb = ImageIO.read(new File(imgpath + "/vids/" + id + "/" + f + ".png"));
+                        mc[0].drawImage(0, 0, bb);
+                    } catch (Exception e) {
+                        getLogger().warning("error reading image, last frame: " + f + "max frame:" + m[0] + " error : " + e);
+                    }
+
                     if (f >= m[0] - 1) {
                         //System.out.println("m[0]: "+m[0]);
                         f = 0;
                     }
                     framedict.put(id, f + 1);
-                } catch (Exception e) {getLogger().warning(e.toString());}
+                } catch (Exception e) {
+                    getLogger().warning(e.toString());
+                }
             } else if (bufferdict.containsKey(id)) {
                 mc[0].drawImage(0, 0, (BufferedImage) bufferdict.get(id));
             }
@@ -128,6 +138,7 @@ public final class Bettermaps extends JavaPlugin implements Listener {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
     private static void web() throws IOException {
         System.out.println("web");
         port(4567);
@@ -138,7 +149,7 @@ public final class Bettermaps extends JavaPlugin implements Listener {
             ClassLoader classloader = Bettermaps.class.getClassLoader();
             InputStream is = classloader.getResourceAsStream("home.html");
 
-            return  new BufferedReader(new InputStreamReader(is))
+            return new BufferedReader(new InputStreamReader(is))
                     .lines().collect(Collectors.joining("\n"));
         });
 
@@ -146,37 +157,100 @@ public final class Bettermaps extends JavaPlugin implements Listener {
         post("/upload", "multipart/form-data", (request, response) -> {
 
 
-                String location = "mapimg/temp";          // the directory location where files will be stored
+            String location = "mapimg/temp";          // the directory location where files will be stored
 
-                MultipartConfigElement multipartConfigElement = new MultipartConfigElement(location);
-                request.raw().setAttribute("org.eclipse.jetty.multipartConfig",
-                        multipartConfigElement);
-                String id = IOUtils.toString(request.raw().getPart("id").getInputStream());
-                System.out.println(id);
-                Part uploadedFile = request.raw().getPart("media");
-                if (uploadedFile.getContentType().contains("image")){
-                    System.out.println("image Upload");
-                    ImageIO.write(Thumbnailator.createThumbnail(ImageIO.read(uploadedFile.getInputStream()),128,128),"png",new File("mapimg/images/" + id + ".png"));
-                    uploadedFile.delete();
-                    for (File subfile : new File("mapimg/vids/"+id).listFiles()) {
+            MultipartConfigElement multipartConfigElement = new MultipartConfigElement(location);
+            request.raw().setAttribute("org.eclipse.jetty.multipartConfig",
+                    multipartConfigElement);
+            String id = IOUtils.toString(request.raw().getPart("id").getInputStream());
+            System.out.println(id);
+            Part uploadedFile = request.raw().getPart("media");
+            if (uploadedFile.getContentType().contains("image")) {
+                System.out.println("image Upload");
+
+                ImageIO.write(Thumbnails.of(uploadedFile.getInputStream()).size(128, 128).keepAspectRatio(false).outputQuality(1.0).outputFormat("png").asBufferedImage(), "png", new File("mapimg/images/" + id + ".png"));
+                uploadedFile.delete();
+                try {
+                    for (File subfile : new File("mapimg/vids/" + id).listFiles()) {
                         subfile.delete();
                     }
-                    new File("mapimg/vids/"+id).delete();
-                    return "image upload OK";
+                    new File("mapimg/vids/" + id).delete();
+                } catch (Exception ignored) {
                 }
-                if (!uploadedFile.getContentType().contains("video")){return "not a video or image upload";}
-                Path out = Paths.get("mapimg/temp/" + id + ".mp4");
-                try (final InputStream in = uploadedFile.getInputStream()) {
-                    Files.copy(in, out);
-                    uploadedFile.delete();
-                }
-                try {extractFrames("mapimg/temp/" + id + ".mp4", 128, 128, 20, id);} catch (IOException e) {throw new RuntimeException(e);}
-                multipartConfigElement = null;
+                return "image upload OK";
+            }
+            if (!uploadedFile.getContentType().contains("video")) {
+                return "not a video or image upload";
+            }
+            Path out = Paths.get("mapimg/temp/" + id + ".mp4");
+            try (final InputStream in = uploadedFile.getInputStream()) {
+                Files.copy(in, out);
                 uploadedFile.delete();
+            }
+            Files.createDirectories(Paths.get("mapimg/vids/" + id));
+            try {
+                extractFramez("mapimg/temp/" + id + ".mp4", 128, 128, 20, id);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            multipartConfigElement = null;
+            uploadedFile.delete();
             return "video upload OK";
         });
     }
 
+
+        public static void extractFramez (String videoFilePath,int width, int height, double frameRate, String id) throws
+        IOException, JCodecException {
+            try (SeekableByteChannel channel = NIOUtils.readableFileChannel(videoFilePath)) {
+                FrameGrab grab = FrameGrab.createFrameGrab(channel);
+                double videoFrameRate = grab.getVideoTrack().getMeta().getTotalFrames() / grab.getVideoTrack().getMeta().getTotalDuration();
+                int frameInterval = (int) Math.round(videoFrameRate / frameRate);
+                try {
+                    for (File subfile : new File("mapimg/vids/" + id).listFiles()) {
+                        subfile.delete();
+                    };} catch (Exception ignored) {}
+                int frameNumber = 0;
+                int savedFrameNumber = 0;
+                Picture f;
+                int totalframenum = grab.getVideoTrack().getMeta().getTotalFrames();
+                while (null != (f = grab.getNativeFrame())) {
+                    if (frameNumber % frameInterval == 0) {
+                        int finalSavedFrameNumber = savedFrameNumber;
+                        saveFrame(f, finalSavedFrameNumber, id, width, height);
+                        savedFrameNumber++;
+                    }
+                    frameNumber++;
+                }
+
+            } finally {
+                System.out.println("done");
+                Files.deleteIfExists(Path.of(videoFilePath));
+            }
+        }
+        private static void saveFrame (Picture picture,int frameNumber, String id,int width, int height){
+            try {
+                BufferedImage bufferedImage = Thumbnails.of(AWTUtil.toBufferedImage(picture))
+                        .size(width, height)
+                        .keepAspectRatio(false)
+                        .outputQuality(1.0)
+                        .outputFormat("png")
+                        .asBufferedImage();
+
+                File outputDir = new File("mapimg/vids/" + id);
+                if (!outputDir.exists()) {
+                    outputDir.mkdirs();
+                }
+
+                File outputFile = new File(outputDir, frameNumber + ".png");
+                ImageIO.write(bufferedImage, "png", outputFile);
+                System.out.println("Saved frame_" + frameNumber + ".png");
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to save frame_" + frameNumber + ".png", e);
+            }
+        }
+    }
+    /*
     public static void extractFrames(String videoFilePath,int width,int height, double frameRate,String id) throws IOException, JCodecException {
         SeekableByteChannel channel = null;
         try {
@@ -190,18 +264,24 @@ public final class Bettermaps extends JavaPlugin implements Listener {
             int savedFrameNumber = 0;
             while (null != (picture = grab.getNativeFrame())) {
                 if (frameNumber % frameInterval == 0) {
+
                     int finalSavedFrameNumber = savedFrameNumber;
                     Picture finalPicture = picture;
-                    scheduler.execute(()->{
-                            BufferedImage bufferedImage = Thumbnailator.createThumbnail(AWTUtil.toBufferedImage(finalPicture), width, height);
-                    File outputfile = new File("mapimg/vids/" + id + "/", +finalSavedFrameNumber + ".png");
-                    try {
-                        ImageIO.write(bufferedImage, "png", outputfile);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    System.out.println("Saved frame_" + finalSavedFrameNumber + ".png");
-                    });
+                    scheduler.execute(()-> {
+                        BufferedImage bufferedImage = null;
+                        try {
+                            bufferedImage = Thumbnails.of(AWTUtil.toBufferedImage(finalPicture)).size(128, 128).keepAspectRatio(false).outputQuality(1.0).outputFormat("png").asBufferedImage();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        File outputfile = new File("mapimg/vids/" + id + "/", +finalSavedFrameNumber + ".png");
+                                try {
+                                    ImageIO.write(bufferedImage, "png", outputfile);
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                                System.out.println("Saved frame_" + finalSavedFrameNumber + ".png");
+                            });
                         savedFrameNumber++;
                 }
                 frameNumber++;
@@ -214,9 +294,9 @@ public final class Bettermaps extends JavaPlugin implements Listener {
             }
         }
     }
-    /*private static void vid2img(@NotNull File videoFile, Size resolution, double targetFrameRate, String id) throws IOException {
+    private static void vid2img(@NotNull String videoFilestr, int width , int height, double targetFrameRate, String id) throws IOException {
         ArrayList<BufferedImage> images = new ArrayList<>();
-
+        File videoFile = new File(videoFilestr);
         VideoCapture videoCapture = new VideoCapture(videoFile.getAbsolutePath());
         Files.delete(videoFile.toPath());
 
@@ -240,7 +320,7 @@ public final class Bettermaps extends JavaPlugin implements Listener {
             }
             if (frameCount % frameSkipInterval == 0) {
                 Mat resizedFrame = new Mat();
-                opencv_imgproc.resize(frame, resizedFrame, resolution);
+                opencv_imgproc.resize(frame, resizedFrame, new Size(width, height));
                 byte[] mob = new byte[(int) resizedFrame.arraySize()];
                 opencv_imgcodecs.imencode(".png", resizedFrame, mob);
                 BufferedImage image = ImageIO.read(new ByteArrayInputStream(mob));
@@ -256,5 +336,6 @@ public final class Bettermaps extends JavaPlugin implements Listener {
             try {ImageIO.write(images.get(i), "png", new File( "mapimg/vids/"+id+"/"+ i + ".png"));} catch (Exception e){System.out.println("error writing image");}
 
         }
-    }*/
+    }
 }
+*/
